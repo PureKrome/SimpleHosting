945e5dba906853745d0485e1e2ef20bdd052e654
Pure Krome <pure.krome@world-domination.com.au>
2020-11-21 14:27:12 +1100

    :lipstick:

--- src/WorldDomination.SimpleHosting.SampleWebApplication/Controllers/WeatherForecastController.cs
+++ src/WorldDomination.SimpleHosting.SampleWebApplication/Controllers/WeatherForecastController.cs
@@ -1,58 +1,26 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using WorldDomination.SimpleHosting.SampleWebApplication.Services;
 
 namespace WorldDomination.SimpleHosting.SampleWebApplication.Controllers
 {
-    //[ApiController]
-    //[Route("[controller]")]
-    //public class WeatherForecastController : ControllerBase
-    //{
-    //    private readonly IWeatherService _weatherService;
-
-    //    public WeatherForecastController(IWeatherService weatherService)
-    //    {
-    //        _weatherService = weatherService ?? throw new ArgumentNullException(nameof(weatherService));
-    //    }
-
-    //    [HttpGet]
-    //    public async Task<IEnumerable<WeatherForecast>> Get()
-    //    {
-    //        return await _weatherService.GetWeatherAsync();
-    //    }
-    //}
-
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private readonly IWeatherService _weatherService;
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(IWeatherService weatherService)
         {
-            _logger = logger;
+            _weatherService = weatherService ?? throw new ArgumentNullException(nameof(weatherService));
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return await _weatherService.GetWeatherAsync();
         }
     }
 }
