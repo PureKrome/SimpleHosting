db977a18c3e84427b55b252c5e43c9fd3273129f
Ben Hyrman <ben@hyr.mn>
2021-03-21 23:58:09 -0400

    Update logger T in HostedService2
    I'm guessing this was a copy-paste typo and `HostedService2` was meant to take in an `ILogger<HostedService2>` and not an `ILogged<HostedService1>`

--- src/WorldDomination.SimpleHosting.SampleWebApplication/HostedService2.cs
+++ src/WorldDomination.SimpleHosting.SampleWebApplication/HostedService2.cs
@@ -4,7 +4,7 @@ namespace WorldDomination.SimpleHosting.SampleWebApplication
 {
     public class HostedService2 : CustomHostedService
     {
-        public HostedService2(ILogger<HostedService1> logger) : base("HostedService-2 (e.g. Database migrations)", logger)
+        public HostedService2(ILogger<HostedService2> logger) : base("HostedService-2 (e.g. Database migrations)", logger)
         {
         }
     }
